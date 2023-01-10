2793bc7cdf8892cf20b8372d70e013a17feaa2cb
Michael Ericson <maerics@gmail.com>
2023-01-10 16:59:02 -0700

    Fix MustExec error cases

--- exec.go
+++ exec.go
@@ -1,12 +1,28 @@
 package goutil
 
 import (
+	"bytes"
 	"os/exec"
+	"strings"
 
 	"github.com/maerics/golog"
 )
 
 func MustExec(command string, args ...string) string {
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
 	cmd := exec.Command(command, args...)
-	return string(golog.Must1(cmd.Output()))
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		cmdline := strings.Join(append([]string{command}, args...), " ")
+		format := "command %q failed: %v"
+		args := []any{cmdline, err}
+		if strings.TrimSpace(stderr.String()) != "" {
+			format += "\n\n%v"
+			args = append(args, stderr.String())
+		}
+		golog.Fatalf(format, args...)
+	}
+	return stdout.String()
 }
